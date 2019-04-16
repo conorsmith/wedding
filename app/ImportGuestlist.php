11b131d62325cffe82a1aa704b26a9e230fcf1d5
Conor Smith <conorjbsmith@gmail.com>
2019-04-16 14:39:36 +0100

    Improve guestlist importer

--- app/ImportGuestlist.php
+++ app/ImportGuestlist.php
@@ -3,12 +3,71 @@
 namespace ConorSmith\Wedding;
 
 use Ramsey\Uuid\Uuid;
+use RandomLib\Factory;
 use Sabre\Xml\Service;
+use SecurityLib\Strength;
 use TheIconic\NameParser\Parser;
 
 final class ImportGuestlist
 {
     public function __invoke(string $path): void
+    {
+        $rows = $this->readSpreadsheet($path);
+
+        $this->truncateTables();
+
+        $this->createGuestsFromSpreadsheetRows($rows);
+
+        $this->generateInvites();
+    }
+
+    private function createGuestsFromSpreadsheetRows(array $rows): void
+    {
+        $parser = new Parser();
+
+        foreach ($rows as $row) {
+            $name = $parser->parse($row[0]);
+
+            $guest = new Guest;
+            $guest->id = Uuid::uuid4();
+            $guest->first_name = $name->getFirstname();
+            $guest->last_name = $name->getLastname();
+            $guest->email = $row[2] ?? "";
+            $guest->phone = $row[3] ?? "";
+            $guest->address = $row[4] ?? "";
+            $guest->save();
+        }
+    }
+
+    private function generateInvites(): void
+    {
+        $guests = Guest::all();
+
+        $factory = new Factory;
+        $generator = $factory->getGenerator(new Strength(Strength::MEDIUM));
+
+        foreach ($guests as $guest) {
+            $invite = new Invite([
+                'id'         => Uuid::uuid4(),
+                'guest_a'    => $guest->id,
+                'note'       => "",
+                'access_key' => $generator->generateString(
+                    256,
+                    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                ),
+            ]);
+            $invite->save();
+        }
+    }
+
+    private function truncateTables(): void
+    {
+        Guest::query()->delete();
+        Relationship::query()->delete();
+        Invite::query()->delete();
+    }
+
+    private function readSpreadsheet(string $path): array
     {
         $contents = file_get_contents($path);
 
@@ -29,21 +88,6 @@ final class ImportGuestlist
 
         array_shift($rows);
 
-        Guest::query()->delete();
-
-        $parser = new Parser();
-
-        foreach ($rows as $row) {
-            $name = $parser->parse($row[0]);
-
-            $guest = new Guest;
-            $guest->id = Uuid::uuid4();
-            $guest->first_name = $name->getFirstname();
-            $guest->last_name = $name->getLastname();
-            $guest->email = $row[2] ?? "";
-            $guest->phone = $row[3] ?? "";
-            $guest->address = $row[4] ?? "";
-            $guest->save();
-        }
+        return $rows;
     }
 }
