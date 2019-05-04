3d0ec329c2bd4e1f8729009c165ca91a67401133
Conor Smith <conorjbsmith@gmail.com>
2019-05-04 11:51:11 +0100

    Add site mode to splash page

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -1,17 +1,15 @@
 <!doctype html>
 <html class="no-js" lang="">
+@inject('siteMode', 'ConorSmith\Wedding\SiteMode')
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Conor & Steph</title>
+  <title>{{ $siteMode->getNames() }}</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-<!--[if lte IE 9]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-<![endif]-->
 
 <div class="container">
   <div style="text-align: center;"><img src="/img/splash.jpg"></div>
