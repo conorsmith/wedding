9c7a74337cd192f0a6ec551a869a76eb1ed33415
Conor Smith <conorjbsmith@gmail.com>
2019-05-12 20:10:28 +0100

    Change index from splash to landing page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('splash');
-});
+Route::get('/', ShowLandingPage::class);
 
 Route::get("/invite/{id}", ViewInvite::class);
 Route::post("/rsvp/{id}", SubmitRsvp::class);
