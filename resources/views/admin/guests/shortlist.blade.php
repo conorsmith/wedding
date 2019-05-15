983f8d17f3064ca771d6388bc19d0d45286d1c46
Conor Smith <conorjbsmith@gmail.com>
2019-05-15 21:39:53 +0100

    Improve button copy

--- resources/views/admin/guests/shortlist.blade.php
+++ resources/views/admin/guests/shortlist.blade.php
@@ -21,7 +21,7 @@
         @include('admin.guests.basicRow')
 
         <td style="text-align: right; width: 165px;">
-          <a href="#" class="btn btn-block btn-sm btn-light border js-invite" style="{{ $guest->is_invited ? "display: none;" : "" }}" data-guest-id="{{ $guest->id }}">Awaiting Guestlist</a>
+          <a href="#" class="btn btn-block btn-sm btn-light border js-invite" style="{{ $guest->is_invited ? "display: none;" : "" }}" data-guest-id="{{ $guest->id }}">Add to Guestlist</a>
           <a href="#" class="btn btn-block btn-sm btn-success border js-uninvite" style="margin-top: 0; {{ !$guest->is_invited ? "display: none" : "" }}" data-guest-id="{{ $guest->id }}"><i class="far fa-check-circle"></i> On Guestlist</a>
         </td>
 
