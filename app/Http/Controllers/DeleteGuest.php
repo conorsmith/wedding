e36dcda3809e41599ca26482b5f962274de81d4d
Conor Smith <conorjbsmith@gmail.com>
2019-05-15 21:52:41 +0100

    Fix delete action

--- app/Http/Controllers/DeleteGuest.php
+++ app/Http/Controllers/DeleteGuest.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 namespace ConorSmith\Wedding\Http\Controllers;
 
 use ConorSmith\Wedding\Guest;
+use DB;
+use Illuminate\Http\Request;
 
 final class DeleteGuest
 {
-    public function __invoke(string $id)
+    public function __invoke(Request $request, string $id)
     {
-        Guest::find($id)->delete();
+        $guest = Guest::find($id);
+
+        if ($guest->hasPartner()) {
+            $request->session()->flash('error', "You must remove this guest's partner before deleting them");
+            return redirect("/admin/guests/{$id}");
+        }
+
+        DB::transaction(function () use ($guest) {
+            $invite = $guest->getInvite();
+            $invite->delete();
+            $guest->delete();
+        });
 
         return redirect("/admin/guests");
     }
