e8bd17a445d3cdc2b2fbc34379c032843624673d
Conor Smith <conorjbsmith@gmail.com>
2019-05-23 20:22:07 +0100

    Finalise email copy

--- resources/views/emails/invite-message.blade.php
+++ resources/views/emails/invite-message.blade.php
@@ -1,14 +1,14 @@
 @inject('siteMode', 'ConorSmith\Wedding\SiteMode')
 
-<h2>Hey {{ $invite->guestA->first_name }}{{ $invite->isForTwoGuests() ? " & " . $invite->guestB->first_name : "" }},</h2>
+<h2>Dear {{ $invite->guestA->first_name }}{{ $invite->isForTwoGuests() ? " & " . $invite->guestB->first_name : "" }},</h2>
 
-<p>We're getting married this summer and <strong>you're invited!</strong></p>
+<p>We request the pleasure of your company at our wedding this summer.</p>
 
 <table>
   <tr>
     <td align="center">
       <p>
-        <a href="http://www.{{ $siteMode->getDomainName() }}/invite/{{ $invite->id }}?key={{ $invite->access_key }}" class="button">Open your invite</a>
+        <a href="http://www.{{ $siteMode->getDomainName() }}/invite/{{ $invite->id }}?key={{ $invite->access_key }}" class="button">Please click here to open your invite</a>
       </p>
     </td>
   </tr>
